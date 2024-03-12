1259c5c75dfec86fab987da7c776b025d8bf9e8a
dbghtjs112 <64840322+dbghtjs112@users.noreply.github.com>
2024-03-12 12:49:52 +0900

    0312_JPA - 테스트 생성

---
+++ src/main/java/com/jpa/basic/domain/Member.java
@@ -0,0 +1,19 @@
+package com.jpa.basic.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity // JPA가 관리할 객체 ( DB 테이블과 동일 )
+@Getter @Setter // Entitiy에는 ToStirng 같이 쓰면 오류가 날수도 있어 가급적 사용X (@Date도 안씀)
+//@ToString(of = {"id","name","age"})
+//@Table("myMember") <- 클래스명과 동일하면 작성 안해도됨
+public class Member {
+    @Id // primary key 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 값 자동 생성
+    private Long id;
+    private String name;
+    private int age;
+
+}
---
+++ src/test/java/com/jpa/basic/domain/MemberTest.java
@@ -0,0 +1,7 @@
+package com.jpa.basic.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberTest {
+
+}
